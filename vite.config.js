6a74f51f1beed1a49c2a98bfb5b631e5a6ce132b
Moodie Foodie <moodiefoodiegithub@gmail.com>
2025-01-29 18:55:21 +0800

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/moodiefoodie/",
   plugins: [vue(), reactRefresh()],
   publicPath: process.env.NODE_ENV === "production" ? "/moodiefoodie/" : "/",
 });
